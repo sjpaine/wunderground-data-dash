de80120b72b2cddaab9eab5727e90dc97c1a13da
Sean Paine <sjpaine@gmail.com>
2018-01-16 18:16:51 +0000

    Initial Dev

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+weatherHistory = {'url': 'http://api.wunderground.com/api/',
+         'apiKey': 'XX',
+         'url2': '/history_20180105',
+         'url3': '/q/pws:ISHANKIL2.json'}
+
+
+weather = {'url': 'http://api.wunderground.com/api/',
+         'apiKey': 'XX',
+         'url2': '',
+         'url3': '/conditions/q/pws:ISHANKIL2.json'}
+
+#http://api.wunderground.com/api/5c42475225e3cbe4/history_20180105/q/pws:ISHANKIL2.json
+
+
+testResponse = ''
---
+++ main.py
@@ -0,0 +1,88 @@
+ #!/usr/bin/python3 
+
+import config as cfg
+import APIKeys as keys
+import urllib3
+import json
+
+from collections import OrderedDict
+from flask import Flask
+from flask import jsonify
+from flask import request
+app = Flask(__name__)
+
+@app.route("/",methods=['GET'])
+
+def main():
+#Build up input url
+	theMeasures = request.args.get('theMeasures')
+	print (request.args)
+	if cfg.testResponse != '':
+		Soutput = parseJsonWunderground(cfg.testResponse)
+	else:
+		Surl = cfg.weatherHistory['url'] + keys.APIKeys['WundergroundKey'] + cfg.weatherHistory['url2'] + cfg.weatherHistory['url3']
+		http = urllib3.PoolManager()
+		print(Surl)
+		r = http.request('GET',Surl)
+		j = json.loads(r.data.decode('utf-8'))
+		return parseJsonWunderground(j,theMeasures)
+		
+
+# {
+#   "graph" : {
+#     "title": "Body measurements",
+#     "datasequences" : [
+#       {
+#         "title" : "Chest Measurements",
+#         "datapoints" : [
+#           { "title" : "1-3-2012", "value" : 45.5 },
+#           { "title" : "1-10-2012", "value" : 45.5 },
+#           { "title" : "1-17-2012" },
+#           { "title" : "1-24-2012", "value" : 44.5 }
+#         ]
+#       },
+#       {
+#         "title" : "Narrowest waist point",
+#         "datapoints" : [
+#           { "title" : "1-3-2012", "value" : 38.6 },
+#           { "title" : "1-10-2012", "value" : 38.1 },
+#           { "title" : "1-17-2012", "value" : 38 },
+#           { "title" : "1-24-2012", "value" : 38 }
+#         ]
+#       }
+#     ]
+#   }
+# }
+
+def parseJsonWunderground(s1,s2):
+#Fix up the json string to send to Datadog.
+	print (s2)
+	output = []
+	graph=OrderedDict()
+	graph['title'] = 'Daily History'
+	datasequences = OrderedDict()
+	graph['datasequences'] = datasequences
+
+	for days in s1['history']['observations']:
+		for key in days:
+			#Check if required
+			if key in s2:
+				if key not in datasequences:
+					datasequences[key] = OrderedDict({'title':key,'datapoints':[]})
+				theDate = days['date']
+				temp = {'title': theDate['mon']+'-'+theDate['mday']+'-'+theDate['year']+'-'+theDate['hour']+'-'+theDate['min'],'value':days[key]}
+				datasequences[key]['datapoints'].append(temp)
+
+	#print(graph)
+	return jsonify({'graph':graph})
+
+def is_number(s):
+    try:
+        complex(s) # for int, long, float and complex
+    except ValueError:
+        return False
+
+    return True
+
+if __name__ == "__main__":
+	main()
