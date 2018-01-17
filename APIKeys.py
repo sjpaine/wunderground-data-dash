7ca2b346866c4c107bc0517472939d0cda898e01
Sean Paine <sjpaine@gmail.com>
2018-01-17 22:12:40 +0000

    Key Update

--- APIKeys.py
+++ APIKeys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 
-APIKeys = {'WundergroundKey':'5c42475225e3cbe4',
-		'DataDogMainKey': '7abc5654c2ca17e94a22e565f0994e91',
-		'DataDogAppKey': 'c0025d03c6b0a8c30bc9a05d0f94bc8701fe6c82'}
+APIKeys = {'WundergroundKey':'i',
+		'DataDogMainKey': 'r',
+		'DataDogAppKey': 'c'}
